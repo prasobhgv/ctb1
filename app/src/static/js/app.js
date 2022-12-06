46266f3f6219c5c6870a55826efa48de50e79fa3
prasobhgv <10627752@mydbs.ie>
2022-12-06 23:31:23 +0000

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add to List'}
+                        {submitting ? 'Adding...' : 'Add-UP!'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
