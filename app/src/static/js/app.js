cd36ad16ea42bf9879365dc89d34537fd66dba57
prasobhgv <116641501+prasobhgv@users.noreply.github.com>
2022-12-13 00:20:45 +0000

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Cool! You're Up-to-Date :)</p>
+                <p className="text-center">You've not tasks left to be finished !</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : '!-UPDATE-!'}
+                        {submitting ? 'Adding...' : 'UPDATE'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
